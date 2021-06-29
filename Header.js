4af6d968fa9d465820d7e026889237c365ab65ae
JawadRafique <39618516+JawadRafique@users.noreply.github.com>
2021-06-30 01:10:08 +0500

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,195 @@
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    makeStyles,
+    Button,
+    IconButton,
+    Drawer,
+    MenuItem,
+    Container,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+const headersData = [
+    {
+        label: "Home",
+        href: "/",
+    },
+    {
+        label: "About",
+        href: "/about",
+    },
+    {
+        label: "Users",
+        href: "/users",
+    },
+];
+
+const useStyles = makeStyles(() => ({
+    header: {
+        backgroundColor: "#400CCC",
+        // paddingRight: "79px",
+        // paddingLeft: "118px",
+        "@media (max-width: 900px)": {
+            // paddingLeft: 0,
+            // paddingRight: 0,
+        },
+    },
+    logo: {
+        fontFamily: "Work Sans, sans-serif",
+        fontWeight: 600,
+        color: "#FFFEFE",
+        textAlign: "left",
+    },
+    menuButton: {
+        fontFamily: "Open Sans, sans-serif",
+        fontWeight: 700,
+        size: "18px",
+        marginLeft: "38px",
+    },
+    toolbar: {
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    drawerContainer: {
+        padding: "20px 30px",
+    },
+}));
+
+export default function Header() {
+    const { header, logo, menuButton, toolbar, drawerContainer } = useStyles();
+
+    const [state, setState] = useState({
+        mobileView: false,
+        drawerOpen: false,
+    });
+
+    const { mobileView, drawerOpen } = state;
+
+    const history = useHistory();
+
+    useEffect(() => {
+        const setResponsiveness = () => {
+            return window.innerWidth < 900
+                ? setState((prevState) => ({ ...prevState, mobileView: true }))
+                : setState((prevState) => ({
+                      ...prevState,
+                      mobileView: false,
+                  }));
+        };
+
+        setResponsiveness();
+
+        window.addEventListener("resize", () => setResponsiveness());
+
+        return () => {
+            window.removeEventListener("resize", () => setResponsiveness());
+        };
+    }, []);
+
+    const displayDesktop = () => {
+        return (
+            <Container>
+                <Toolbar className={toolbar}>
+                    {femmecubatorLogo}
+                    <div>{getMenuButtons()}</div>
+                </Toolbar>
+            </Container>
+        );
+    };
+
+    const displayMobile = () => {
+        const handleDrawerOpen = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: true }));
+        const handleDrawerClose = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: false }));
+
+        return (
+            <Toolbar className={toolbar}>
+                <div>{femmecubatorLogo}</div>
+                <IconButton
+                    {...{
+                        edge: "start",
+                        color: "inherit",
+                        "aria-label": "menu",
+                        "aria-haspopup": "true",
+                        onClick: handleDrawerOpen,
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
+
+                <Drawer
+                    {...{
+                        anchor: "right",
+                        open: drawerOpen,
+                        onClose: handleDrawerClose,
+                    }}
+                >
+                    {/* <ul className={drawerContainer}>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                    </ul> */}
+                    <div className={drawerContainer}>{getDrawerChoices()}</div>
+                </Drawer>
+            </Toolbar>
+        );
+    };
+
+    const getDrawerChoices = () => {
+        return headersData.map(({ label, href }) => {
+            return (
+                <Link
+                    {...{
+                        // component: RouterLink,
+                        to: href,
+                        color: "inherit",
+                        style: { textDecoration: "none" },
+                        key: label,
+                    }}
+                >
+                    <MenuItem>{label}</MenuItem>
+                </Link>
+            );
+        });
+    };
+
+    const femmecubatorLogo = (
+        <Typography variant="h6" component="h1" className={logo}>
+            Femmecubator
+        </Typography>
+    );
+
+    const getMenuButtons = () => {
+        return headersData.map(({ label, href }) => {
+            return (
+                <Button
+                    {...{
+                        key: label,
+                        color: "inherit",
+                        to: href,
+                        // component: RouterLink,
+                        className: menuButton,
+                    }}
+                    onClick={() => history.push(href)}
+                >
+                    {label}
+                </Button>
+            );
+        });
+    };
+
+    return (
+        <header>
+            <AppBar className={header}>
+                {mobileView ? displayMobile() : displayDesktop()}
+            </AppBar>
+        </header>
+    );
+}
